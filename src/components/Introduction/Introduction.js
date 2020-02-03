6e2d56568f38b7c599c81fe5f0eedd0779a93cad
Aivaras <lukauskasaivaras@gmail.com>
2020-02-03 21:59:45 +0200

    css done

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Introduction.css';
+import Logo from './swed.png';
+
 
 function Introduction() {
     return (
         <div style={{ backgroundColor: '#FBF2EA' }}>
             <div className='Intro'>
-                <h1 style={{ color: '#EE7023' }}>Automobilių lizingas</h1>
-                <h2 style={{ fontSize: '35px' }}>Mėgaukitės vairuodami savo naująjį automobilį!</h2>
+                <img src={Logo} className="logo" />
+                <h1>Automobilių lizingas</h1>
+                <h2 style={{ fontSize: '35px', color: '#EE7023' }}>Mėgaukitės vairuodami savo naująjį automobilį!</h2>
                 <p>Jei svarstote įsigyti naują automobilį – lizingas yra puikus sprendimas.</p>
                 <ul>
                     <li>Palūkanų norma įsigyjant naują automobilį nuo 2,29 % + 6 mėnesių Euribor (ne mažiau kaip 0 %).</li>
@@ -15,6 +18,7 @@ function Introduction() {
                     <li>Palankesnės sąlygos įsigyjant aplinkai draugišką automobilį.</li>
                 </ul>
                 <p>Netinkamas finansinių įsipareigojimų vykdymas didina Jūsų finansavimosi kaštus ir gali daryti neigiamą įtaką Jūsų kredito istorijai. Jei sutartis bus nutraukta Jums ją iš esmės pažeidus, gali būti pradėtas priverstinis skolos išieškojimas ir galite prarasti ne tik jau sumokėtas įmokas, bet ir teises į finansuojamą turtą.</p>
+                <button className='introBtn'><strong>Plačiau</strong></button>
             </div>
         </div>
     )
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,6 +4,7 @@ import './Nav.css';
 function Nav(props) {
     return (
         <div className='main'>
+            <h1>Automobilio lizingo paraiška</h1>
             <div className='NavContainer'>
                 <span className='active'>1</span>
                 <span className={props.q1show === true ? '' : 'active'}>2</span>
